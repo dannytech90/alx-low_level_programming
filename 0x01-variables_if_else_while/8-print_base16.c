3026d0416fd4889c8f31871f5eccfce7777ecd46
dannytech90 <profjaywon@gmail.com>
2023-08-14 20:01:06 -0700

    print base 16 in lower case

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function prints all the numbers of base 16 in lowercase,
+ * followed by a new line. It uses the putchar function to achieve this.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char hex_digit;
+	for (hex_digit = '0'; hex_digit <= '9'; hex_digit++)
+	{
+        putchar(hex_digit);
+	}
+	for (hex_digit = 'a'; hex_digit <= 'f'; hex_digit++)
+	{
+	putchar(hex_digit);
+	}
+	putchar('\n');
+	return (0);
+}
