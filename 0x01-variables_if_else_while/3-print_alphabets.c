f55911bed749c3e9e42c97f680acd6840a81e433
dannytech90 <profjaywon@gmail.com>
2023-08-14 19:14:40 -0700

    fix betty

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the lowercase alphabet, followed by the uppercase alphabet,
+ * and then a new line. It uses the putchar function to achieve this.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char lowercase;
 	char uppercase;
 
-    for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
-    {
-	putchar(lowercase);
-    }
-    for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
-    {
-	putchar(uppercase);
-    }
-    	putchar('\n');
-
-    return (0);
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		putchar(lowercase);
+	}
+	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+	{
+		putchar(uppercase);
+	}
+	putchar('\n');
+	return (0);
 }
